e1820f9c2a299697592f5347896f223cc43ddc52
bsorrentino <bartolomeo.sorrentino@gmail.com>
2018-12-18 19:01:23 +0100

    update

--- client/ts/examples/simple.ts
+++ client/ts/examples/simple.ts
@@ -1,8 +1,9 @@
 
 import {interval, from, of } from 'rxjs';
-import { delay, concatMap, tap } from 'rxjs/operators';
+import { concatMap, delay, tap } from 'rxjs/operators';
 
 import * as rxmarbles from '../lib/marble-core';
+import { _p_observer } from '../lib/marble-rxjs';
 
 var currentExample:rxmarbles.ExampleState;
 
@@ -15,37 +16,24 @@ window.addEventListener('load',  () => {
     let shapes$:rxmarbles.ExampleCode = {
         autoPlay: true,
         exec: () => {
+            var i = 0;
 
-            marbles.logger.onStart( {   
-                id:producerId, 
-                name:name, 
-                //parentId:parentProducerId, 
-                createdByValue:true, 
-                isIntermediate:false
-            });
-
-            of( 'A', 'B', 'C', 'D', 'E', 'F' )
-            .pipe( delay(500), concatMap( e => of(e).pipe( delay(1000) )))
+            //of( 'A', 'B', 'C', 'D', 'E', 'F' )
+            interval( 1000 )
+            .pipe(  _p_observer( 'interval(1000) ' , '$result') )
+            .pipe(  concatMap( e =>  of(e).pipe( delay(1000) , _p_observer( 'delay(1000) '+(++i) , '$result') ) ) )
+            .pipe( _p_observer( '$result') )
             .subscribe( val => {
+                console.log(val);
+            }, 
+            err => {
 
-                marbles.logger.onValue({   
-                    id:producerId, 
-                    name:name, 
-                    //parentId:parentProducerId, 
-                    value:val, 
-                    });   
+            },
+            () => {  
 
-            }, err => {},
-            () => { 
-                marbles.logger.onComplete( { 
-                            id:producerId, 
-                            name:name, 
-                            //parentId:parentProducerId, 
-                            }) 
-                })                 
+            })                 
                 ;
             
-
             return () => {}
         }
         
---
+++ client/ts/lib/marble-rxjs.ts
@@ -0,0 +1,216 @@
+import {  Observable, PartialObserver, MonoTypeOperatorFunction, Observer } from 'rxjs';
+import {  tap } from 'rxjs/operators';
+import { 
+    Sample, 
+    SampleInfo,
+    SamplerLogger,
+    SampleItemType
+ } from './marble-handler';
+import { ObserveOnOperator } from 'rxjs/internal/operators/observeOn';
+
+
+export function _p_observer<T>( id:string, parentId?:string ):MonoTypeOperatorFunction<T> {
+
+    return (source:Observable<T>) => new Observable<T>( observer =>  {
+
+        return source.subscribe(_p_observer_internal( observer, id, parentId ) );
+    })
+}
+
+let _time = () => (performance) ? performance.now() : Date.now() ;
+
+
+
+function _p_observer_internal<T>( observer:Observer<T> , id:string, parentId?:string ):PartialObserver<T> 
+{
+    const event:Sample = {
+        type: SampleItemType.Start,
+        time: _time(),
+        id:id,
+        parentId:parentId,
+        name:id,
+        createdByValue: true,
+        isIntermediate:false
+    };
+    window.dispatchEvent( new CustomEvent('rxmarbles.event', { detail: event } ));
+
+    return {
+        next: (v:any) => {
+            const event:Sample = {
+                type: SampleItemType.Value,
+                time: _time(),
+                id:id,
+                parentId:parentId,
+                name:id,
+                value: v
+            };
+            window.dispatchEvent( new CustomEvent('rxmarbles.event', { detail: event } ));
+            observer.next(v);
+        },
+        error: (err:any) => {
+            console.log( id, parentId, name, err );
+            const event:Sample = {
+                type: SampleItemType.Error,
+                time: _time(),
+                id:id,
+                parentId:parentId,
+                name:id,
+                err:err
+            };
+            window.dispatchEvent( new CustomEvent('rxmarbles.event', { detail: event } ));
+            observer.error(err);
+        },
+        complete: () => {
+            const event:Sample = {
+                type: SampleItemType.Complete,
+                time: _time(),
+                id:id,
+                parentId:parentId,
+                name:id
+            };
+            window.dispatchEvent( new CustomEvent('rxmarbles.event', { detail: event } ));
+            observer.complete();
+        }
+    }
+};
+
+
+/// JS PROXY 
+
+/*
+function _proxy_subscriber<T extends Object>( id:string, parentId:string, input:T ):T {
+    
+    return  new Proxy( input, { 
+        get: (target: T, name: PropertyKey, receiver: any) => {
+
+            if( name in target ) {
+                switch( name ) {
+                    case 'next': 
+                        return ( v:any ) => { 
+                            console.log( id, parentId, name, v );
+                            const event:Sample = {
+                                type: SampleItemType.Value,
+                                time: Date.now(),
+                                id:id,
+                                parentId:parentId,
+                                name:id,
+                                value: v
+                            };
+                            window.dispatchEvent( new CustomEvent('rxmarbles.event', { detail: event } ));
+                            return (<any>target)[name]( v );
+                        }
+                    case 'error':
+                        return ( err:any ) =>  { 
+
+                            console.log( id, parentId, name, err );
+                            const event:Sample = {
+                                type: SampleItemType.Error,
+                                time: Date.now(),
+                                id:id,
+                                parentId:parentId,
+                                name:id,
+                                err:err
+                            };
+                            window.dispatchEvent( new CustomEvent('rxmarbles.event', { detail: event } ));
+                            
+                            return (<any>target)[name]( err );
+                        }
+                    case 'complete':
+                        return ( v:any ) => { 
+                            console.log( id, parentId, name, v );
+
+                            const event:Sample = {
+                                type: SampleItemType.Complete,
+                                time: Date.now(),
+                                id:id,
+                                parentId:parentId,
+                                name:id
+                            };
+                            window.dispatchEvent( new CustomEvent('rxmarbles.event', { detail: event } ));
+                            return (<any>target)[name]( v );
+                        }
+                    case 'remove':
+                        return ( v:any ) => { 
+                            console.log( id, parentId, name, v );
+
+                            const event:Sample = {
+                                type: SampleItemType.Stop,
+                                time: Date.now(),
+                                id:id,
+                                parentId:parentId,
+                                name:id
+                            };
+                            window.dispatchEvent( new CustomEvent('rxmarbles.event', { detail: event } ));
+                            return (<any>target)[name]( v );
+                        }
+                    case 'add':
+                        return ( v:any ) => { 
+                            console.log( id, parentId, name, v );
+
+                            const event:Sample = {
+                                type: SampleItemType.Start,
+                                time: Date.now(),
+                                id:id,
+                                parentId:parentId,
+                                name:id,
+                                createdByValue: true,
+                                isIntermediate:false
+                            };
+                            window.dispatchEvent( new CustomEvent('rxmarbles.event', { detail: event } ));
+                            return (<any>target)[name];
+                        }
+                    default: 
+                        {
+                            let v = (<any>target)[name];
+                            console.log( id, parentId, '==>' + String(name), v );
+                            return v;
+                        }
+                }
+                
+            }
+        }
+    });
+}
+
+function _proxy_subscribe<T extends Object>( id:string, parentId:string, input:T ):T {
+
+
+    return  new Proxy( input, { 
+        apply: (target: T, thisArg: any, argArray?: any) => {
+            
+            if( argArray && argArray.length == 1 && typeof(argArray[0]) == "object")  {
+                let args = [ _proxy_subscriber(id, parentId, argArray[0]) ];
+        
+                return  (<any>target).apply( thisArg, args );
+            }
+            
+            return (<any>target).apply( thisArg, argArray );
+        }
+    })
+}
+
+export function _p<T extends Object>( id:string, parentId:string, input:T ):T {
+
+    return  new Proxy( input, { 
+        get: (target: T, name: PropertyKey, receiver: any) => {
+
+            if( name in target ) {
+                let result =  (<any>target)[name];
+
+                if( name === "subscribe" ) {
+                    return _proxy_subscribe( id, parentId, result);
+                }
+                
+                return result;
+            }
+        },
+        apply: (target: T, thisArg: any, argArray?: any) => {
+
+            let result =  (<any>target).apply( thisArg, argArray);
+
+            return _p( id, parentId, result);
+        }
+    })
+}
+
+*/
