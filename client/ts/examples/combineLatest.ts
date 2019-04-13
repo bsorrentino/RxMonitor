060324f3b5d9145eaa48a9c7853d9ae24c4ab4fd
Bartolomeo Sorrentino <bsorrentino@Bartolomeos-iMac.local>
2019-04-13 23:24:40 +0200

    refactor samples

--- client/ts/examples/combineLatest.ts
+++ client/ts/examples/combineLatest.ts
@@ -7,27 +7,17 @@ import { startExample, watch } from '../sdk/marble-rxjs';
 
 let combineLatest$ = () => {
 
-let watchChild = <T>( id:string ) => watch<T>(id, 'combineLatest');
+  const w$ = <T>( id?:string ) => watch<T>( '$result', id );   
 
-
-  //timerOne emits first value at 1s, then once every 4s
-const timerOne = timer(1000, 4000).pipe( watchChild('timerOne') );
-//timerTwo emits first value at 2s, then once every 4s
-const timerTwo = timer(2000, 4000).pipe( watchChild('timerTwo') );
-//timerThree emits first value at 3s, then once every 4s
-const timerThree = timer(3000, 4000).pipe( take(2), watchChild('timerThree'));
+  const timerOne = timer(1000, 4000).pipe( w$('timerOne') );
+  const timerTwo = timer(2000, 4000).pipe( w$('timerTwo') );
+  const timerThree = timer(3000, 4000).pipe( take(2), w$('timerThree'));
 
 //when one timer emits, emit the latest values from each timer as an array
-const combined = combineLatest(timerOne, timerTwo, timerThree).pipe( take(10), watch('combineLatest'));
+const combined = combineLatest(timerOne, timerTwo, timerThree).pipe( take(10), w$());
 
 return combined.subscribe(
   ([timerValOne, timerValTwo, timerValThree]) => {
-    /*
-      Example:
-    timerOne first tick: 'Timer One Latest: 1, Timer Two Latest:0, Timer Three Latest: 0
-    timerTwo first tick: 'Timer One Latest: 1, Timer Two Latest:1, Timer Three Latest: 0
-    timerThree first tick: 'Timer One Latest: 1, Timer Two Latest:1, Timer Three Latest: 1
-  */
     console.log(
       `Timer One Latest: ${timerValOne},
      Timer Two Latest: ${timerValTwo},
@@ -38,10 +28,6 @@ return combined.subscribe(
 
 }
 
-
-/**
- * 
- */
 window.addEventListener('load',  () => {   
 
     document.addEventListener( "click", ()=> {
--- client/ts/examples/delay.ts
+++ client/ts/examples/delay.ts
@@ -2,22 +2,18 @@ import { interval, of } from 'rxjs';
 import { concatMap, delay, take } from 'rxjs/operators';
 import { startExample, watch } from '../sdk/marble-rxjs';
 
-
 let delay$ = () => {
-   
-    let source = 
-      interval( 1000 ).pipe(take(20), watch( 'interval()' , '$result') )
-      .pipe( concatMap( e =>  of(e).pipe( delay(1000) , watch( 'delay()' , '$result') ) ) )
-      .pipe( watch( '$result') )
-    
-    
-    return source.subscribe( val => console.log(val));
+  const w$ = <T>( id?:string ) => watch<T>( '$result', id );
+
+  let source = 
+      interval( 1000 ).pipe(take(20), w$( 'interval()' ) )
+      .pipe( concatMap( e =>  of(e).pipe( delay(1000) , w$( 'delay()') ) ) )
+      .pipe( w$() )
+      
+  return source.subscribe( val => console.log(val));
    
 };
 
-/**
- * 
- */
 window.addEventListener('load',  () => { 
   
     document.addEventListener( "click", ()=> {
