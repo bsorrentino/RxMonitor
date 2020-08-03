83a2ddd2e31e3b758782ef4ce4fd2bef5b011f21
bartolomeo sorrentino <bartolomeo.sorrentino@gmail.com>
2020-08-03 20:38:09 +0200

    remove p5 import from commn

--- client/ts/lib/p5/common.ts
+++ client/ts/lib/p5/common.ts
@@ -1,14 +1,8 @@
-import p5 from "p5"
-
 export const DEFAULT_BACKGROUND = 51
 
 export const DEFAULT_FPS = 30
 
 export type Boundary = { left:number ; right:number }
-
-export function p5boundary( k$:p5 ):Boundary { 
-    return {left:0, right:k$.width} 
-}
 
 export class Queue<T> {
 
