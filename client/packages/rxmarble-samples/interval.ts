86b83590294bcc549fa29e95a1e298adfd180b3b
bsorrentino <bartolomeo.sorrentino@gmail.com>
2020-09-23 23:51:50 +0200

    update

--- client/packages/rxmarble-samples/interval.ts
+++ client/packages/rxmarble-samples/interval.ts
@@ -6,7 +6,7 @@ import { watch } from '@bsorrentino/rxmarble-sdk';
 const interval$ = () => {
   const w$ = <T>( id?:string ) => watch<T>( '$result', id );   
 
-  const example = interval(1000).pipe( w$('interval(1s)'), take(10) );
+  const example = interval(1000).pipe( take(10), w$('interval(1s)') );
 
   return example.pipe( w$() ).subscribe( console.debug );
    
